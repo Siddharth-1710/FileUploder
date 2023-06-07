dbbb34ef02c1f5a98377c06e88915dcb16d03917
Siddharth-1710 <siddharthdesai1710@gmail.com>
2023-06-07 20:43:44 +0530

    Updates

---
+++ precache-manifest.57207c9a1d2cdeb7390deb2f5776c631.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6e1de588a2759eb4f9d3",
+    "url": "/FileUploder/static/css/main.d69b8974.chunk.css"
+  },
+  {
+    "revision": "6e1de588a2759eb4f9d3",
+    "url": "/FileUploder/static/js/main.6e1de588.chunk.js"
+  },
+  {
+    "revision": "54087e177e7c5a193df0",
+    "url": "/FileUploder/static/js/1.54087e17.chunk.js"
+  },
+  {
+    "revision": "f7702b7c6e5bf1fe2c7a",
+    "url": "/FileUploder/static/js/2.f7702b7c.chunk.js"
+  },
+  {
+    "revision": "228558f850e8c36d7e21",
+    "url": "/FileUploder/static/js/runtime~main.228558f8.js"
+  },
+  {
+    "revision": "bbc9ed726ab16da79c71c644d54dff51",
+    "url": "/FileUploder/index.html"
+  }
+];
